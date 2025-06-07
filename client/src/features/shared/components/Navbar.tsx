5699d2fe43778413864ea6371af7cc9a062e32c6
bsegers <briansegs@gmail.com>
2025-06-07 12:25:56 -0400

    Set up auth and login

--- client/src/features/shared/components/Navbar.tsx
+++ client/src/features/shared/components/Navbar.tsx
@@ -1,8 +1,11 @@
-import { Home, Search } from "lucide-react";
+import { Home, Search, User } from "lucide-react";
 import { ThemeToggle } from "./ThemeToggle";
 import Link from "./ui/Link";
+import { useCurrentUser } from "@/features/auth/hooks/useCurrentUser";
 
 export default function Navigation() {
+  const { currentUser } = useCurrentUser();
+
   const navLinkClassName =
     "rounded-lg p-2 text-lg hover:bg-neutral-100 dark:hover:bg-neutral-800";
 
@@ -29,6 +32,20 @@ export default function Navigation() {
         <Search className="h-6 w-6" />
         Search
       </Link>
+
+      {currentUser ? (
+        <div>Signed In</div>
+      ) : (
+        <Link
+          to="/login"
+          variant="ghost"
+          className={navLinkClassName}
+          activeProps={{ className: activeNavLinksClassName }}
+        >
+          <User className="h-6 w-6" />
+          Sign in
+        </Link>
+      )}
 
       <ThemeToggle />
     </nav>
