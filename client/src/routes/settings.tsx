8f68c08f475d84d2d0dbcf581a2fd6bcbcc14900
bsegers <briansegs@gmail.com>
2025-06-08 11:09:33 -0400

    Implemented changing password

--- client/src/routes/settings.tsx
+++ client/src/routes/settings.tsx
@@ -1,4 +1,5 @@
 import { ChangeEmailDialog } from "@/features/auth/components/ChangeEmailDialog";
+import { ChangePasswordDialog } from "@/features/auth/components/ChangePasswordDialog";
 import { useCurrentUser } from "@/features/auth/hooks/useCurrentUser";
 import { Button } from "@/features/shared/components/ui/Button";
 import Card from "@/features/shared/components/ui/Card";
@@ -52,6 +53,10 @@ function SettingsPage() {
     {
       label: currentUser?.email,
       component: <ChangeEmailDialog />,
+    },
+    {
+      label: "Change Password",
+      component: <ChangePasswordDialog />,
     },
     {
       label: "Sign out of your account",
