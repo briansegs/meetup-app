1b70f0bf7d5005b46dcbd099c4274ec6a3b91bf9
bsegers <briansegs@gmail.com>
2025-06-13 10:08:28 -0400

    Displayed following and followers lists

--- client/src/features/users/types.ts
+++ client/src/features/users/types.ts
@@ -1,9 +1,14 @@
 import { User } from "@meetup-app/server/database/schema";
 
+type UserWithFollowCount = User & {
+  followersCount: number;
+  followingCount: number;
+};
+
 type UserWithHostedExperiences = User & {
   hostedExperiencesCount: number;
 };
 
 export type UserForList = User;
 
-export type UserForDetails = UserWithHostedExperiences;
+export type UserForDetails = UserWithFollowCount & UserWithHostedExperiences;
